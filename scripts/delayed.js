0075d1483b5acf5ccd04838f6e4cf929534f0b8c
ISD_6851\PrashantK <PrashantK@hla.hongleong.com.my>
2026-04-21 15:11:14 +0530

    day 5 - project structure exploration

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,1 @@
-// scripts/delayed.js
-// Everything here loads AFTER 3 seconds
-// Perfect for: Analytics, chat, ads, third party widgets
-
-// Example: Google Analytics would go here
-// import('./analytics.js');
-
-// Example: Chat widget would go here  
-// loadChatWidget();
-
-console.log('Delayed scripts loaded after 3 seconds!');
+// add delayed functionality here
